aadbd24f25415ab4fec4df815bffae6852f5a807
Quentin <q.smith1@yahoo.com>
2017-12-07 23:09:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo() {
+  return "Happy holidays, you!"
+}
+function happyHolidayTo(holiday, name) {
+  holiday = "Independence Day"
+  name = you
+  return "Happy ${holiday}, ${name}!"
+}
